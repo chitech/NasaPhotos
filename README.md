a65f841241cb7fa1a263b68685cfe9c0e13eaf78
Christine B Clayton <chicagotechpro@gmail.com>
2023-10-03 01:08:19 -0400

    Update styles for modern code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NasaPhotos
-NASA Photo of the Day. API, javascript
+NASA Photo of the Day is a simple Javascript project that displays the latest photos from an API.
+## Technologies
+Vanilla Javascript and HTML
--- assets/scripts.js
+++ assets/scripts.js
@@ -1,72 +1,38 @@
 // Get photos past 30 days
-var today = new Date();
-var thirtyDays = [];
-var todayFormatted=moment(today).format("YYYY-MM-DD");
-console.log(todayFormatted);
-var url = [];
-var prevDay= moment(today).subtract(1, "days").format("YYYY-MM-DD");
-console.log(prevDay);
-var count;
-for (count=0; count < 8; count++) {
-  var prevDay= moment(prevDay).subtract(1, "days").format("YYYY-MM-DD");
-thirtyDays.push(prevDay);
-  console.log(prevDay);
-}
-function get(url) {
-  // Return a new promise.
-  return new Promise(function(resolve, reject) {
-    // Request
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
+const today = moment().format("YYYY-MM-DD");
+const thirtyDays = [];
+const url = 'https://api.nasa.gov/planetary/apod';
+const apiKey = process.env.API_KEY;
 
-    req.onload = function() {
-      //Check status of request
-      if (req.status == 200) {
-        // Complete promise with the response text
-        resolve(req.response);
-      }
-      else {
-        // Reject error text
-        reject(Error(req.statusText));
-      }
-    };
-
-    // Manage errors
-    req.onerror = function() {
-      reject(Error("Network Error"));
-    };
-
-    // Send the request
-    req.send();
-  });
+for (let i = 0; i < 8; i++) {
+  const prevDay = moment(today).subtract(i + 1, "days").format("YYYY-MM-DD");
+  thirtyDays.push(prevDay);
 }
 
-   //Run Get api url
-              var output = "";
-              for (i=0; i<thirtyDays.length; i++) {
-              currDate = thirtyDays[i];
-              get('https://api.nasa.gov/planetary/apod?api_key=lY5L86uXs31eIA8nsAIKth2W3MeSD43RU0kiY5hc&date=' + currDate
-              ).then(function(response) {
-              return JSON.parse(response);
-              }).then(function(response) {
-              console.log(response.url);
-  //Thirty days list output
-              output += `
-                      <div class="col-md-6">
-                        <div class="well">
-                           <img src="${response.url}"/>
-                           <div class="caption">
-                            <h3 class="text-center">${response.title}</h3>
-                              <ul class="form-group">
-                                <li class="list-group-item"><strong>Photo Day:</strong>${response.date}</li>
-                                <li class="list-group-item">${response.explanation}</li>
-                                <li class="list-group-item"><strong>Photo By:</strong>${response.copyright}</li>
+async function getPhotoData(date) {
+  const response = await fetch(`${url}?api_key=${apiKey}&date=${date}`);
+  const data = await response.json();
+  return data;
+}
 
-                           </div> 
-                        </div>
-                      </div>
-                      `;
-                    document.getElementById("photos").innerHTML = output;
-                }); 
+async function getPhotos() {
+  const photoData = await Promise.all(thirtyDays.map(getPhotoData));
+  const output = photoData.map(data => `
+    <div class="col-md-6">
+      <div class="well">
+        <img src="${data.url}"/>
+        <div class="caption">
+          <h3 class="text-center">${data.title}</h3>
+          <ul class="form-group">
+            <li class="list-group-item"><strong>Photo Day:</strong>${data.date}</li>
+            <li class="list-group-item">${data.explanation}</li>
+            <li class="list-group-item"><strong>Photo By:</strong>${data.copyrigh}</li>
+          </ul>
+        </div> 
+      </div>
+    </div>
+  `).join('');
+  document.getElementById("photos").innerHTML = output;
+}
 
-      }
+getPhotos();
